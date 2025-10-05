bfaad05dfee7a1bf17c0543820bb2d8ae336fc42
Riddhi <riddhikabra0206@gmail.com>
2025-10-06 02:21:35 +0530

    Setting dev/test/custom spring profiles

--- src/main/java/com/rk/example/ExampleApplication.java
+++ src/main/java/com/rk/example/ExampleApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(ExampleApplication.class, args);
+
+        var app = new SpringApplication(ExampleApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
+        var ctx = app.run(args);
 //        MyFirstClass myFirstClass = new MyFirstClass();
         MyFirstClass myFirstClass = ctx.getBean("myFirstBean",MyFirstClass.class);
         System.out.println(myFirstClass.sayHello());
